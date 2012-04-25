16b4238133beab33e80a3454395cbd479fc205b5
Anders M. Andersen <anders.m.andersen@gmail.com>
2012-04-25 19:56:13 +0200

    Removed (and changed) WURFL API Key

--- WURFL.php
+++ WURFL.php
@@ -3,8 +3,9 @@
 require_once 'WURFL/Client/Client.php';
 // Create a configuration object
 $config = new WurflCloud_Client_Config();
-// Set your WURFL Cloud API Key
-$config->api_key = '425902:roc8EOQzi9eIFDWx3fwVbAlhGjXay7HL';
+// Set your WURFL Cloud API Key (add your own key)
+//$config->api_key = '12345:abcdefgabcdefgabcdefgabcdefg';
+include "fragments/WURFLKey.php";
 // Create the WURFL Cloud Client
 $client = new WurflCloud_Client_Client($config);
 // Detect your device
