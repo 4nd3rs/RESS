98cfd878e9fb6f9454a60ce7fea2a263b17271af
Anders M. Andersen <anders.m.andersen@gmail.com>
2012-05-04 00:18:55 +0200

    removed h26 test

--- js/feature.js
+++ js/feature.js
@@ -1,7 +1,5 @@
-$("#width").html(window.innerWidth);
+ $("#width").html(window.innerWidth);
 
 Modernizr.touch ?  $("#touch").html("Yes") : $("#touch").html("No");
 
 Modernizr.cssanimations ?  $("#cssanim").html("Yes") : $("#cssanim").html("No");
-
-Modernizr.video.h264 ?  $("#h264").html(Modernizr.video.h264) : $("#h264").html("No");
