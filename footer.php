c9202d847c42c683377112df49efcc4b0bc485c3
Anders M. Andersen <anders.m.andersen@gmail.com>
2012-05-04 21:16:35 +0200

    Changed bio

--- footer.php
+++ footer.php
@@ -4,18 +4,20 @@
     <div class="container">
 
         <div class="row">
-            <div class="span4">
-                <h3>About</h3>
-                <p>This page is created by <a href="http://twitter.com/andmag">Anders Andersen</a> as a part of a demo of how RESS can be used.</p>
+            <div class="span6">
+                <h3 id="about">About</h3>
+                <p>This page is created by <a href="http://twitter.com/andmag">Anders M. Andersen</a> as a practical demo of how RESS can be used.</p>
             </div>
-            <div class="span4">
-                <h3>Resources</h3>
-                <p><a href="">Twitter Bootstrap</a></p>
+            <div class="span6">
+                <h3>Bio</h3>
+                <p>Anders M. Andersen works, lives and plays in Stockholm, Sweden. He has been doing mobile development since WML was
+                    hot and are currently trying to get that "responsive thing" to work better for mobile devices. Catch up
+                    with Anders on Twitter as <a href="http://twitter.com/andmag">@andmag</a>.</p>
             </div>
-            <div class="span4">
+            <!--<div class="span4">
                 <h3>@netmag</h3>
-                <p>is an awesomemagazine</p>
-            </div>
+                <p>This site is created as a part</p>
+            </div>-->
         </div>
     </div>
 </div>
