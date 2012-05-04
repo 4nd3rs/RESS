49d4860960d0dcabbb543a9b4fb60e5c0aa5bad7
Anders M. Andersen <anders.m.andersen@gmail.com>
2012-05-04 21:39:11 +0200

    Added a text intro to the site

---
+++ fragments/content.php
@@ -0,0 +1,13 @@
+<h1>About this site</h1>
+<p>
+    This is a demo site that shows how we can use server side together with responsive web design techniques to optimize
+    the experience. The site uses three techniques:
+</p>
+<ol>
+    <li><strong>Responsive images</strong> - A device with a small screen will get a smaller image, a device with a larger screen gets larger images.</li>
+    <li><strong>Ads</strong> - Smaller ads are displayed on smaller screen, larger ads are displayed on larger screens. Look at the Google adsense ad at the top of the site.</li>
+    <li><strong>Conditional Content Inclusion</strong> - The Twitter and Facebook social widgets are not included for small screens due. Try to load the site on a small screen versus on a large screen.</li>
+</ol>
+<p>
+    Try to resize the window and reload the site at different sizes to see how it works.
+</p>
