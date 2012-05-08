1df71a20cbf37e001b42670ddcc40141c7330e15
Anders M. Andersen <anders.m.andersen@gmail.com>
2012-05-08 20:23:17 +0200

    Some spelling fixes

--- fragments/content.php
+++ fragments/content.php
@@ -6,8 +6,8 @@
 <ol>
     <li><strong>Responsive images</strong> - A device with a small screen will get a smaller image, a device with a larger screen gets larger images.</li>
     <li><strong>Ads</strong> - Smaller ads are displayed on smaller screen, larger ads are displayed on larger screens. Look at the Google adsense ad at the top of the site.</li>
-    <li><strong>Conditional Content Inclusion</strong> - The Twitter and Facebook social widgets are not included for small screens due. Try to load the site on a small screen versus on a large screen.</li>
+    <li><strong>Conditional Content Inclusion</strong> - The Twitter and Facebook social widgets are not included for small screens. Try to load the site in a small browser window versus on a large browser window.</li>
 </ol>
 <p>
-    Try to resize the window and reload the site at different sizes to see how it works.
+    Try to resize the window and reload the site at different sizes to see how it all works!
 </p>
